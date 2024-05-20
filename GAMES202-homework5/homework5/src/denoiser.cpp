037de0466e32865cc330856037db4082d01adecb
fushengyang <1147596448@qq.com>
2024-05-21 00:40:47 +0800

    联合双边滤波

--- GAMES202-homework5/homework5/src/denoiser.cpp
+++ GAMES202-homework5/homework5/src/denoiser.cpp
@@ -46,6 +46,41 @@ Buffer2D<Float3> Denoiser::Filter(const FrameInfo &frameInfo) {
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
             // TODO: Joint bilateral filter
+            //ÁªºÏË«±ßÂË²¨
+            //¾ØÐÎ
+            int max_x = std::min(width - 1, x + kernelRadius);
+            int max_y = std::min(height - 1, y + kernelRadius);
+            int min_x = std::max(0, x - kernelRadius);
+            int min_y = std::min(0, y - kernelRadius);
+
+            Float3 c_normal = frameInfo.m_normal(x, y);
+            Float3 c_pos = frameInfo.m_position(x, y);
+            Float3 c_depth = frameInfo.m_depth(x, y);
+            Float3 c_color = frameInfo.m_beauty(x, y);
+
+    /*            float m_alpha = 0.2f;
+            float m_sigmaPlane = 0.1f;
+            float m_sigmaColor = 0.6f;
+            float m_sigmaNormal = 0.1f;
+            float m_sigmaCoord = 32.0f;
+            float m_colorBoxK = 1.0f;*/
+
+            for (size_t i = min_x; i < max_x; i++) {
+                for (size_t j = min_y; j < max_y; j++) {
+
+                    Float3 o_normal = frameInfo.m_normal(i,j);
+                    Float3 o_depth = frameInfo.m_depth(i, j);
+                    Float3 o_color = frameInfo.m_beauty(i, j);
+
+                    o_normal = () / (2 * m_sigmaPlane * m_sigmaPlane);
+
+
+
+                }
+            }
+
+
+
             filteredImage(x, y) = frameInfo.m_beauty(x, y);
         }
     }
